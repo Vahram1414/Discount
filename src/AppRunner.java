b9f1038d323e7ed90c21002479063e6f8415fc29
Vahram Alickhanyan <anton-sviridov92@mail.ru>
2024-02-29 22:54:16 +0300

    my version3

--- src/AppRunner.java
+++ src/AppRunner.java
@@ -1,42 +1,38 @@
+import com.sun.source.tree.BreakTree;
+
 import java.util.List;
-import java.lang.ClassNotFoundException;
-import java.util.Scanner;
 
 public class AppRunner {
 
     public static int amount;
-    public static int price;
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        {
-            System.out.println("введите сумму заказа");
-            int amount = sc.nextInt();
-//            char operation = sc.next().charAt(0);
-            if (amount > 1000.0 && amount <= 4999.0) {
-                price = (int) (amount * 0.05);
-                System.out.println(amount * 0.05);
-            } else if (amount > 5000.0) {
-                price = (int) (amount * 0.1);
-                System.out.println(amount * 0.1);
-            }
-        }
+        DiscountRule discountRule = new DiscountRule (0.0, Double.MAX_VALUE, 0.05);
+        calculateDiscount(amount);
     }
 
-    public double calculateDiscount(double amount) {
+
+    public static double calculateDiscount(double amount) {
 
         List<DiscountRule> discountRules = List.of(
-                new DiscountRule(0.0, 999.0, 0.05),
-                new DiscountRule(1000.0, Double.MAX_VALUE, 0.1)
+                new DiscountRule(0.0, 999.0, 0.0),
+                new DiscountRule(1000.0, 4999.0, 0.05),
+                new DiscountRule(5000.0, Double.MAX_VALUE, 0.1)
         );
-        return 0.0;
+        for (int i = 0; i < 3; i++) {
+            if (DiscountRule.isApplicable) {
+                DiscountRule.calculateDiscount(amount);
+            }
+        }
+        return amount;
     }
 
     static class DiscountRule {
+        public static boolean isApplicable;
         private Double minAmount;
         private Double maxAmount;
 
-        public Double discount;
+        public static Double discount;
 
         public DiscountRule(Double minAmount, Double maxAmount, Double discount) {
             this.minAmount = minAmount;
@@ -45,11 +41,15 @@ public class AppRunner {
         }
 
         public boolean isApplicable(double amount) {
+
             return amount >= minAmount && amount <= maxAmount;
         }
 
-        public double calculateDiscount(double amount) {
+        public static double calculateDiscount(double amount) {
+
             return amount * discount;
         }
     }
 }
+
+
